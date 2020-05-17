b2a50be94c6d828c49602664033f19209c662d6f
BluemediaGER <oliver@traber-info.de>
2020-05-17 23:14:52 +0200

    Initial commit

---
+++ src/main/java/de/traber_info/home/ldap2azure/Ldap2Azure.java
@@ -0,0 +1,12 @@
+package de.traber_info.home.ldap2azure;
+
+/**
+ * Main class for ldap2azure. Performs the first initialisation of all components.
+ *
+ * @author Oliver Traber
+ */
+public class Ldap2Azure {
+
+
+
+}
